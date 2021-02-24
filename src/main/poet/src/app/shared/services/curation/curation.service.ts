c3d907804dd34fe2ae0ffe0fe508cf895649e7ed
iimpulse <garganomx@gmail.com>
2021-02-24 10:40:45 -0500

    cleanup

--- src/main/poet/src/app/shared/services/curation/curation.service.ts
+++ src/main/poet/src/app/shared/services/curation/curation.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { environment } from "../../../../environments/environment";
 import { Observable } from "rxjs";
-import {Disease, TreatmentAnnotation, Publication, UserActivityResponse} from "../../models/models";
+import { Disease, Publication, TreatmentAnnotation, UserActivityResponse } from "../../models/models";
 import { StateService } from "../state/state.service";
 import { map, shareReplay } from "rxjs/operators";
 
@@ -12,13 +12,14 @@ import { map, shareReplay } from "rxjs/operators";
 export class CurationService {
 
 
-  constructor(private httpClient: HttpClient, private stateService: StateService) { }
+  constructor(private httpClient: HttpClient, private stateService: StateService) {
+  }
 
   /**
    * Search for publications and diseases by a query.
    * @param query
    */
-  searchAll(query: string){
+  searchAll(query: string) {
     let parameters: HttpParams = new HttpParams().set('query', query);
     return this.httpClient.get(environment.POET_API_SEARCH_URL, {params: parameters});
   }
@@ -27,7 +28,7 @@ export class CurationService {
    * Get a disease by OMIM id
    * @param id
    */
-  getDisease(id: string): Observable<Disease>{
+  getDisease(id: string): Observable<Disease> {
     return this.httpClient.get<any>(environment.POET_API_DISEASE_ENTITY_URL + id);
   }
 
@@ -35,7 +36,7 @@ export class CurationService {
    * Get a disease by OMIM id
    * @param diseaseRequest a monarch disease object
    */
-  saveDisease(diseaseRequest: any): Observable<any>{
+  saveDisease(diseaseRequest: any): Observable<any> {
     return this.httpClient.put(environment.POET_API_DISEASE_ENTITY_URL, diseaseRequest);
   }
 
@@ -43,7 +44,7 @@ export class CurationService {
    * Get a list of publications associated with a disease OMIM
    * @param id
    */
-  getDiseasePublications(id: string): Observable<Publication[]>{
+  getDiseasePublications(id: string): Observable<Publication[]> {
     return this.httpClient.get<Publication[]>(environment.POET_API_DISEASE_ENTITY_URL + id + "/publications");
   }
 
@@ -51,7 +52,7 @@ export class CurationService {
    * Get a publication by a pubmed id.
    * @param id
    */
-  getPublication(id: string): Observable<Publication>{
+  getPublication(id: string): Observable<Publication> {
     return this.httpClient.get<Publication>(environment.POET_API_PUBLICATION_ENTITY_URL + id);
   }
 
@@ -67,23 +68,22 @@ export class CurationService {
    * Get a list of diseases associated to a publication
    * @param id - ideally a PMID.
    */
-  getPublicationDiseases(id: string): Observable<Disease[]>{
+  getPublicationDiseases(id: string): Observable<Disease[]> {
     return this.httpClient.get<Disease[]>(environment.POET_API_PUBLICATION_ENTITY_URL + id + "/diseases");
   }
 
   /**
    * Get a list of Maxo Annotations
-   * @param publication
    * @param disease
-   * @param ontology
+   * @param publication
    * @param sort
    */
-  getTreatmentAnnotations(disease: Disease, publication: Publication, sort: string): Observable<TreatmentAnnotation[]>{
+  getTreatmentAnnotations(disease: Disease, publication: Publication, sort: string): Observable<TreatmentAnnotation[]> {
     let params;
-    if(sort){
+    if (sort) {
       params = new HttpParams().set("sort", sort);
     }
-    if(publication != null){
+    if (publication != null) {
       return this.httpClient.get<any>(environment.POET_API_MAXO_ANNOTATION + `${disease.diseaseId}/${publication.publicationId}`, {params: params});
     } else {
       return this.httpClient.get<any>(environment.POET_API_MAXO_ANNOTATION + disease.diseaseId, {params: params}).pipe(shareReplay());
@@ -128,7 +128,7 @@ export class CurationService {
    * Get User Activity for curations
    * @param everyone
    */
-  getActivity(everyone: boolean){
+  getActivity(everyone: boolean) {
     const params = new HttpParams().set("all", String(everyone));
     return this.httpClient.get<UserActivityResponse[]>(environment.POET_API_STATISTICS_ACTIVITY_URL, {params: params}).pipe(
       map((response: UserActivityResponse[]) => {
@@ -159,7 +159,7 @@ export class CurationService {
    * @param everyone - a boolean if we should get everyone or the current user
    * @param weeksBack - how many weeks of activity
    */
-  getGroupActivityFeed(everyone: boolean, weeksBack: number){
+  getGroupActivityFeed(everyone: boolean, weeksBack: number) {
     const params = new HttpParams().set("all", String(everyone)).set("weeks", String(weeksBack));
     return this.httpClient.get<UserActivityResponse[]>(environment.POET_API_STATISTICS_ACTIVITY_URL, {params: params}).pipe(
       map((activityMap) => this.reduceToGroupedActivity(activityMap))
@@ -173,12 +173,10 @@ export class CurationService {
    * Activity under an hour gets grouped by day, disease, user, type, action
    * @param activityList
    */
-  reduceToGroupedActivity(activityList){
+  reduceToGroupedActivity(activityList) {
     let activities = [];
     let that = this;
     let dayMap = activityList.reduce(function (r, a) {
-      // Group by days from time
-      // compute difference and round
       let date = new Date(a.localDateTime);
       const dayDifference = that.daysFromNow(date);
       r[dayDifference] = r[dayDifference] || [];
@@ -187,38 +185,34 @@ export class CurationService {
     }, Object.create(null));
 
     Object.keys(dayMap).forEach(daysFromNow => {
-
-      dayMap[daysFromNow] = dayMap[daysFromNow].reduce(function (r, a) {
+      dayMap[daysFromNow] = dayMap[daysFromNow].reduce((r, a) => {
         let disease = a.annotation.annotationSource.disease.diseaseId + "~" +
           a.annotation.annotationSource.disease.diseaseName;
-          r[disease] = r[disease] || [];
-          r[disease].push(a);
+        r[disease] = r[disease] || [];
+        r[disease].push(a);
         return r;
       }, Object.create(null));
 
-
       Object.keys(dayMap[daysFromNow]).forEach(diseaseJoined => {
         const [diseaseId, diseaseName] = diseaseJoined.split("~");
-
-        if(parseInt(daysFromNow) == 0){
+        if (parseInt(daysFromNow) == 0) {
           // Today
-          dayMap[daysFromNow][diseaseJoined] = dayMap[daysFromNow][diseaseJoined].reduce(function (r, a) {
+          dayMap[daysFromNow][diseaseJoined] = dayMap[daysFromNow][diseaseJoined].reduce((r, a) => {
             r[a.user.nickname] = r[a.user.nickname] || [];
             r[a.user.nickname].push(a);
             return r;
           }, Object.create(null));
 
           Object.keys(dayMap[daysFromNow][diseaseJoined]).forEach(user => {
-            dayMap[daysFromNow][diseaseJoined][user] = dayMap[daysFromNow][diseaseJoined][user].reduce(function (r, a) {
+            dayMap[daysFromNow][diseaseJoined][user] = dayMap[daysFromNow][diseaseJoined][user].reduce((r, a) => {
               r[a.annotation.annotationType] = r[a.annotation.annotationType] || [];
               r[a.annotation.annotationType].push(a);
               return r;
             }, Object.create(null));
 
-
             Object.keys(dayMap[daysFromNow][diseaseJoined][user]).forEach(type => {
               // Group by hours from now
-              dayMap[daysFromNow][diseaseJoined][user][type] = dayMap[daysFromNow][diseaseJoined][user][type].reduce(function (r, a) {
+              dayMap[daysFromNow][diseaseJoined][user][type] = dayMap[daysFromNow][diseaseJoined][user][type].reduce((r, a) => {
                 // Group by hours from now
                 let date = new Date(a.localDateTime);
                 const hourDifference = that.hoursFromNow(date);
@@ -229,8 +223,8 @@ export class CurationService {
 
               Object.keys(dayMap[daysFromNow][diseaseJoined][user][type]).forEach(hour => {
                 const hoursFromNow = parseInt(hour);
-                if(hoursFromNow == 0){
-                  dayMap[daysFromNow][diseaseJoined][user][type][hour] = dayMap[daysFromNow][diseaseJoined][user][type][hour].reduce(function (r, a) {
+                if (hoursFromNow == 0) {
+                  dayMap[daysFromNow][diseaseJoined][user][type][hour] = dayMap[daysFromNow][diseaseJoined][user][type][hour].reduce((r, a) => {
                     r[a.curationAction] = r[a.curationAction] || [];
                     r[a.curationAction].push(a);
                     return r;
@@ -255,7 +249,7 @@ export class CurationService {
                     });
                   });
 
-                } else if(hoursFromNow > 0){
+                } else if (hoursFromNow > 0) {
                   const count = dayMap[daysFromNow][diseaseJoined][user][type][hoursFromNow].length;
                   const mostRecent = this.getMostRecentDate(dayMap[daysFromNow][diseaseJoined][user][type][hoursFromNow]);
                   const annotationGrammar = this.annotationGrammar(count);
@@ -276,7 +270,7 @@ export class CurationService {
             });
           });
 
-        } else if(parseInt(daysFromNow) > 0){
+        } else if (parseInt(daysFromNow) > 0) {
           // Days Ago Up to a week
           const mostRecent = this.getMostRecentDate(dayMap[daysFromNow][diseaseJoined]);
           const count = dayMap[daysFromNow][diseaseJoined].length;
@@ -285,7 +279,7 @@ export class CurationService {
             return item.user.nickname;
           }))].join(",");
           let view;
-          if(parseInt(daysFromNow) == 1){
+          if (parseInt(daysFromNow) == 1) {
             view = `${userList} modified ${count} ${annotationGrammar} for ${diseaseName} yesterday.`;
           } else {
             view = `${userList} modified ${count} ${annotationGrammar} for ${diseaseName} ${daysFromNow} days ago.`;
@@ -298,38 +292,33 @@ export class CurationService {
         }
       });
     });
-    return activities.sort((a:any, b:any) => (b.date - a.date));
+    return activities.sort((a: any, b: any) => (b.date - a.date));
   }
 
-  private getMostRecentDate(annotations){
+  private getMostRecentDate(annotations) {
     let dates = annotations.map(a => {
       return new Date(a.localDateTime);
     });
     return new Date(Math.max(...dates));
   }
 
-  private annotationGrammar(count){
+  private annotationGrammar(count) {
     return count > 1 ? "annotations" : "annotation";
   }
 
   private daysFromNow(dateToCheck: Date): number {
     const today = new Date();
-    return Math.round((today.getTime() - dateToCheck.getTime())/(24*3600*1000));
-  }
-
-  private roundToHour(dateToRound): number {
-    let m = 60 * 60 * 1000; // milliseconds in an hour
-    return new Date(Math.round(dateToRound.getTime() / m ) * m).getHours();
+    return Math.round((today.getTime() - dateToCheck.getTime()) / (24 * 3600 * 1000));
   }
 
   private hoursFromNow(dateToCheck): number {
     const now = new Date();
-    return Math.round((now.getTime() - dateToCheck.getTime())/(1000*60*60))
+    return Math.round((now.getTime() - dateToCheck.getTime()) / (1000 * 60 * 60))
   }
 
   private minutesFromNow(dateToCheck): number {
     const now = new Date();
-    return Math.round((now.getTime() - dateToCheck.getTime())/(1000*60))
+    return Math.round((now.getTime() - dateToCheck.getTime()) / (1000 * 60))
   }
 
   private actionLookup(action): string {
@@ -345,7 +334,7 @@ export class CurationService {
    * Get User Activity for curations
    * @param everyone
    */
-  getUserContributions(){
+  getUserContributions() {
     return this.httpClient.get(environment.POET_API_STATISTICS_CONTRIBUTION_URL).pipe(
       map((contributions) => {
         return [{"value": contributions["maxo"], "name": "Medical Action Ontology"},
@@ -358,7 +347,7 @@ export class CurationService {
   /**
    * Get Annotation Counts by disease id or all if none
    */
-  getAnnotationCounts(diseaseId: string): any{
+  getAnnotationCounts(diseaseId: string): any {
     return this.httpClient.get(environment.POET_API_STATISTICS_ANNOTATION_URL + diseaseId);
   }
 }
