83394b96970f51e4e9e02ecaa66cef4e99ea673b
iimpulse <michael.gargano@jax.org>
2021-10-14 15:16:38 -0700

    Export service addition for treatments

--- src/main/java/org/monarchinitiative/poet/service/ExportService.java
+++ src/main/java/org/monarchinitiative/poet/service/ExportService.java
@@ -3,9 +3,11 @@ package org.monarchinitiative.poet.service;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.monarchinitiative.poet.model.entities.PhenotypeAnnotation;
+import org.monarchinitiative.poet.model.entities.TreatmentAnnotation;
 import org.monarchinitiative.poet.model.entities.Version;
 import org.monarchinitiative.poet.model.enumeration.AnnotationStatus;
 import org.monarchinitiative.poet.repository.PhenotypeAnnotationRepository;
+import org.monarchinitiative.poet.repository.TreatmentAnnotationRepository;
 import org.monarchinitiative.poet.repository.VersionRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +23,15 @@ public class ExportService {
     private final AnnotationService annotationService;
     private final VersionRepository versionRepository;
     private final PhenotypeAnnotationRepository phenotypeAnnotationRepository;
+    private final TreatmentAnnotationRepository treatmentAnnotationRepository;
 
-    public ExportService(AnnotationService annotationService, VersionRepository versionRepository, PhenotypeAnnotationRepository phenotypeAnnotationRepository) {
+    public ExportService(AnnotationService annotationService, VersionRepository versionRepository,
+                         PhenotypeAnnotationRepository phenotypeAnnotationRepository,
+                         TreatmentAnnotationRepository treatmentAnnotationRepository) {
         this.annotationService = annotationService;
         this.versionRepository = versionRepository;
         this.phenotypeAnnotationRepository = phenotypeAnnotationRepository;
+        this.treatmentAnnotationRepository = treatmentAnnotationRepository;
     }
 
     public void exportHPOAnnotations(PrintWriter writer, CSVFormat format){
@@ -54,14 +60,26 @@ public class ExportService {
     public void releaseAnnotations(){
         // Create a new release version
         Version version = createNewReleaseVersion();
-        List<PhenotypeAnnotation> annotations = phenotypeAnnotationRepository.findAllByStatus(AnnotationStatus.ACCEPTED);
+        List<PhenotypeAnnotation> phenotypeAnnotations = phenotypeAnnotationRepository.findAllByStatus(AnnotationStatus.ACCEPTED);
+        List<TreatmentAnnotation> treatmentAnnotations = treatmentAnnotationRepository.findAllByStatus(AnnotationStatus.ACCEPTED);
         // For all accepted phenotype annotations we want to check to see if an official one exists already as official
-        for(PhenotypeAnnotation annotation: annotations){
+        for(PhenotypeAnnotation annotation: phenotypeAnnotations){
             annotation.setStatus(AnnotationStatus.OFFICIAL);
             // If not, change annotation to official status, tag with release
             if(!annotationService.phenotypeAnnotationExists(annotation, null)){
                 annotation.setVersion(version);
                 phenotypeAnnotationRepository.save(annotation);
+                // TODO: Add useractivity update
+            }
+        }
+
+        for(TreatmentAnnotation annotation: treatmentAnnotations) {
+            annotation.setStatus(AnnotationStatus.OFFICIAL);
+            // If not, change annotation to official status, tag with release
+            if (!annotationService.treatmentAnnotationExists(annotation, null)) {
+                annotation.setVersion(version);
+                treatmentAnnotationRepository.save(annotation);
+                // TODO: Add useractivity update
             }
         }
     }
