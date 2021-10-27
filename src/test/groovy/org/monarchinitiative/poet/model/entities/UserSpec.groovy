c66c75c5f1b2fe895fab195e64bcad5743c692a9
iimpulse <michael.gargano@jax.org>
2021-10-27 13:45:37 -0700

    fixing user spec test

--- src/test/groovy/org/monarchinitiative/poet/model/entities/UserSpec.groovy
+++ src/test/groovy/org/monarchinitiative/poet/model/entities/UserSpec.groovy
@@ -11,33 +11,32 @@ class UserSpec extends Specification {
 
     void "test user constructor"(){
         given:
-            def user = new User(authId, nickname, email, orcId, curationRole)
-            def user2 = new User(authId, nickname, email, orcId, curationRole)
+            def user = new User(authId, nickname, email, curationRole)
+            def user2 = new User(authId, nickname, email,curationRole)
         expect:
             user.getAuthId() == authId
             user.getCurationRole() == curationRole
             user.getEmail() == email
             user.getNickname() == nickname
-            user.getOrcId() == orcId
             user.equals(user2)
             user.hashCode() == user2.hashCode()
         where:
-            authId  |   curationRole    |   email   |   nickname    |   orcId
-            "auth|1039" |   CurationRole.POET_CURATOR |   "curator@gmail.com" | "the best curator" | "orcid:029323"
-            "auth|103920" |   CurationRole.POET_ADMIN |   "elevated. @gmail.com" | "even better curator" | "orcid:029344"
+            authId  |   curationRole    |   email   |   nickname
+            "auth|1039" |   CurationRole.POET_CURATOR |   "curator@gmail.com" | "the best curator"
+            "auth|103920" |   CurationRole.POET_ADMIN |   "elevated. @gmail.com" | "even better curator"
 
     }
 
     void "test user set curation role"(){
         given:
-            def user = new User(authId, nickname, email, orcId, curationRole)
+            def user = new User(authId, nickname, email, curationRole)
             user.setCurationRole(expectedRole)
         expect:
             user.getCurationRole() == expectedRole
 
         where:
-        authId  |   curationRole    |   email   |   nickname    |   orcId   |  expectedRole
-        "auth|1039" |   CurationRole.POET_CURATOR |   "curator@gmail.com"    | "the best curator"    | "orcid:029323" | CurationRole.POET_ADMIN
-        "auth|103920" |   null                    |   "elevated. @gmail.com" | "even better curator" | "orcid:029344" | CurationRole.POET_CURATOR
+        authId  |   curationRole    |   email   |   nickname    |   expectedRole
+        "auth|1039" |   CurationRole.POET_CURATOR |   "curator@gmail.com"    | "the best curator"    | CurationRole.POET_ADMIN
+        "auth|103920" |   null                    |   "elevated. @gmail.com" | "even better curator" | CurationRole.POET_CURATOR
     }
 }
