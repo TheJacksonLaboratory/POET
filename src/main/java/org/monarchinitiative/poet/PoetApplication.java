8eef98a21685c5523421e719ca6dc88ceff71ff2
iimpulse <Michael.Gargano@jax.org>
2023-11-29 12:06:52 -0700

    removing unused imports

--- src/main/java/org/monarchinitiative/poet/PoetApplication.java
+++ src/main/java/org/monarchinitiative/poet/PoetApplication.java
@@ -1,19 +1,6 @@
 package org.monarchinitiative.poet;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class PoetApplication {
